a8ddbcfc468afdbda3f849e4c18d44a8f97ef3b4
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-02-03 12:14:10 +0530

    Initial Commit

---
+++ CustomPasscodeLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomPasscodeLock
+//
+//  Created by SAHIL AMRUT AGASHE on 03/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomPasscodeLock/CustomPasscodeLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPasscodeLockApp.swift
+//  CustomPasscodeLock
+//
+//  Created by SAHIL AMRUT AGASHE on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomPasscodeLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
